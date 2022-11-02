fe4f1669d7afb9639e69fe4b471ede3a85ec31a2
Milena Hikari <milenahikari@MacBook-Air-de-Milena.local>
2022-11-02 12:56:04 -0300

    feat: creating new pages

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+import { Pools } from "./src/screens/Pools";
 
 import { THEME } from "./src/styles/theme";
 
@@ -25,7 +25,7 @@ export default function App() {
     <NativeBaseProvider theme={THEME}>
       <AuthContextProvider>
         <StatusBar style="light" backgroundColor="transparent" translucent />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
