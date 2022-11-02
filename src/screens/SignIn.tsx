30189fc432c2eb279264e3561af4ff697bc1c142
Milena Hikari <milenahikari@MacBook-Air-de-Milena.local>
2022-11-02 11:12:01 -0300

    feat: add configs svg transform and types

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
 import { Center, Text } from "native-base";
 
+import Logo from "../assets/logo.svg";
+
 export function SignIn() {
   return (
-    <Center flex={1}>
-      <Text color="black">SignIn</Text>
+    <Center flex={1} bgColor="gray.900">
+      <Logo width={212} height={40} />
     </Center>
   );
 }
