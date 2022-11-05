bb3907d1d294d19815a19e982086f0a252c53f6b
Milena Hikari <milenahikari@MacBook-Air-de-Milena.local>
2022-11-05 15:30:58 -0300

    feat: connecting api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,6 +3,8 @@ import * as Google from "expo-auth-session/providers/google";
 import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
 
+import { api } from "../services/api";
+
 WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
@@ -45,8 +47,22 @@ export function AuthContextProvider({ children }: AutProviderProps) {
     }
   }
 
-  async function signInWithGoogle(access_token: string) {
-    console.log("Token", access_token);
+  async function signInWithGoogle(accessToken: string) {
+    try {
+      setUserLoading(true);
+      const tokenResponse = await api.post("/users", { accessToken });
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${tokenResponse.data.token}`;
+
+      const userInfoResponse = await api.get("/me");
+      setUser(userInfoResponse.data.user);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setUserLoading(false);
+    }
   }
 
   useEffect(() => {
